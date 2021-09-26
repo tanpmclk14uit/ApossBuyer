0d3598c4066d8f4ad8c71df367ac84d5ba94e4af
tanpmclk14uit <tan.lk16.cla@gmail.com>
2021-09-26 23:15:16 +0700

    new push

---
+++ app/src/main/java/com/example/aposs_buyer/uicontroler/fragment/FavoriteFragment.kt
@@ -0,0 +1,24 @@
+package com.example.aposs_buyer.uicontroler.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.aposs_buyer.R
+import com.example.aposs_buyer.databinding.FragmentFavoriteBinding
+
+
+class FavoriteFragment : Fragment() {
+    private lateinit var binding: FragmentFavoriteBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_favorite, container, false)
+        return binding.root
+    }
+
+}
---
+++ app/src/main/java/com/example/aposs_buyer/uicontroler/fragment/HomeFragment.kt
@@ -0,0 +1,23 @@
+package com.example.aposs_buyer.uicontroler.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.aposs_buyer.R
+import com.example.aposs_buyer.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        return binding.root
+    }
+
+}
