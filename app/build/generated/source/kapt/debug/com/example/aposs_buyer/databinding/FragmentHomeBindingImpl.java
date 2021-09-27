cc6d770520618e20b7165948e23ae97cedd3cf59
tanpmclk14uit <tan.lk16.cla@gmail.com>
2021-09-27 19:51:18 +0700

    done home app bar

--- app/build/generated/source/kapt/debug/com/example/aposs_buyer/databinding/FragmentHomeBindingImpl.java
+++ app/build/generated/source/kapt/debug/com/example/aposs_buyer/databinding/FragmentHomeBindingImpl.java
@@ -13,23 +13,25 @@ public class FragmentHomeBindingImpl extends FragmentHomeBinding  {
     private static final android.util.SparseIntArray sViewsWithIds;
     static {
         sIncludes = null;
-        sViewsWithIds = null;
+        sViewsWithIds = new android.util.SparseIntArray();
+        sViewsWithIds.put(R.id.title, 1);
     }
     // views
     @NonNull
-    private final android.widget.FrameLayout mboundView0;
+    private final androidx.coordinatorlayout.widget.CoordinatorLayout mboundView0;
     // variables
     // values
     // listeners
     // Inverse Binding Event Handlers
 
     public FragmentHomeBindingImpl(@Nullable androidx.databinding.DataBindingComponent bindingComponent, @NonNull View root) {
-        this(bindingComponent, root, mapBindings(bindingComponent, root, 1, sIncludes, sViewsWithIds));
+        this(bindingComponent, root, mapBindings(bindingComponent, root, 2, sIncludes, sViewsWithIds));
     }
     private FragmentHomeBindingImpl(androidx.databinding.DataBindingComponent bindingComponent, View root, Object[] bindings) {
         super(bindingComponent, root, 0
+            , (android.widget.TextView) bindings[1]
             );
-        this.mboundView0 = (android.widget.FrameLayout) bindings[0];
+        this.mboundView0 = (androidx.coordinatorlayout.widget.CoordinatorLayout) bindings[0];
         this.mboundView0.setTag(null);
         setRootTag(root);
         // listeners
