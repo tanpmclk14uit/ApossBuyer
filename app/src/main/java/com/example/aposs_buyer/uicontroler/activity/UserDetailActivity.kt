5f545b64bd0fc71c820dc452cb79475f2d7502ea
Bùi Dương Duy Khang <81902132+khangbdd@users.noreply.github.com>
2021-10-06 20:34:29 +0700

    fix sth

--- app/src/main/java/com/example/aposs_buyer/uicontroler/activity/UserDetailActivity.kt
+++ app/src/main/java/com/example/aposs_buyer/uicontroler/activity/UserDetailActivity.kt
@@ -2,11 +2,28 @@ package com.example.aposs_buyer.uicontroler.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
 import com.example.aposs_buyer.R
+import com.example.aposs_buyer.databinding.ActivityUserDetailBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class UserDetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityUserDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user_detail)
+        binding = ActivityUserDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navController = Navigation.findNavController(this, R.id.userDetailFragment)
+        navController.navigateUp()
+        navController.navigate(R.id.userDetailFragment)
     }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        finish()
+    }
+
 }
